302d22a1b8a9b405de200d5a4b457feba97059b8
FENNECK04 <62812847+FENNECK04@users.noreply.github.com>
2025-08-19 17:22:14 +0100

    Add responsive video template

---
+++ newsreader/template-parts/content-video.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Template part for displaying video posts.
+ *
+ * @package Newsreader
+ */
+
+?>
+
+<div class="cs-entry__wrap">
+
+        <?php
+        /**
+         * The csco_entry_wrap_start hook.
+         *
+         * @since 1.0.0
+         */
+        do_action( 'csco_entry_wrap_start' );
+        ?>
+
+        <div class="cs-entry__container">
+
+                <?php
+                /**
+                 * The csco_entry_container_start hook.
+                 *
+                 * @since 1.0.0
+                 */
+                do_action( 'csco_entry_container_start' );
+                ?>
+
+                <?php
+                $media = get_media_embedded_in_content(
+                        apply_filters( 'the_content', get_the_content() ),
+                        array( 'video', 'object', 'embed', 'iframe' )
+                );
+
+                if ( ! empty( $media ) ) {
+                        echo '<div class="entry-video-wrapper">' . $media[0] . '</div>';
+                }
+
+                // Remove first video from content to avoid duplication.
+                $content = get_the_content();
+                if ( ! empty( $media ) ) {
+                        $content = str_replace( $media[0], '', $content );
+                }
+                ?>
+
+                <div class="cs-entry__content-wrap">
+                        <?php
+                        /**
+                         * The csco_entry_content_before hook.
+                         *
+                         * @since 1.0.0
+                         */
+                        do_action( 'csco_entry_content_before' );
+                        ?>
+
+                        <div class="entry-content">
+                                <?php echo apply_filters( 'the_content', $content ); ?>
+                        </div>
+
+                        <?php
+                        /**
+                         * The csco_entry_content_after hook.
+                         *
+                         * @since 1.0.0
+                         */
+                        do_action( 'csco_entry_content_after' );
+                        ?>
+                </div>
+
+                <?php
+                /**
+                 * The csco_entry_container_end hook.
+                 *
+                 * @since 1.0.0
+                 */
+                do_action( 'csco_entry_container_end' );
+                ?>
+
+        </div>
+
+        <?php
+        /**
+         * The csco_entry_wrap_end hook.
+         *
+         * @since 1.0.0
+         */
+        do_action( 'csco_entry_wrap_end' );
+        ?>
+</div>
+
